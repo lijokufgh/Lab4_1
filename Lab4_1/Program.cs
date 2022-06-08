baf868cbe5c7ed9daaaf4d4f31d48434656e3434
lijokufgh <ilygj@mail.ru>
2022-06-08 09:17:42 +0500

    Добавление комментариев.

--- Lab4_1/Program.cs
+++ Lab4_1/Program.cs
@@ -1,18 +1,18 @@
 ﻿namespace Program
 {
-    public abstract class LogaAbstr
+    public abstract class LogaAbstr // Абстрактный класс.
     {
-        protected abstract void Schot(float x, float y, int z);
+        protected abstract void Schot(float x, float y, int z); // Абстрактный метод.
     }
-    public class Loga : LogaAbstr
+    public class Loga : LogaAbstr // Наследование бстрактного класса.
     {
-        public static void Input()
+        public static void Input() // Статический метод вывода информации по входным данным.
         {
             Console.WriteLine("Введите логорифм, основание и до какого числа после запятой округлять.");
         }
-        protected override void Schot(float x, float y, int z)
+        protected override void Schot(float x, float y, int z) // Переопредиление абстрактного метода. Посчёт логарифма с огруглением.
         {
-            Console.WriteLine(Math.Round(Math.Log(x, y), z));
+            Console.WriteLine(Math.Round(Math.Log(x, y), z)); // Посчёт логарифма с огруглением.
         }
         public Loga(float x, float y, int z)
         {
