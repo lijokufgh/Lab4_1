aac3e978749a7ec4b64e0a6105edf3eb4d3d76cc
lijokufgh <ilygj@mail.ru>
2022-06-08 09:13:45 +0500

    Добавьте файлы проекта.

---
+++ Lab4_1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Program
+{
+    public abstract class LogaAbstr
+    {
+        protected abstract void Schot(float x, float y, int z);
+    }
+    public class Loga : LogaAbstr
+    {
+        public static void Input()
+        {
+            Console.WriteLine("Введите логорифм, основание и до какого числа после запятой округлять.");
+        }
+        protected override void Schot(float x, float y, int z)
+        {
+            Console.WriteLine(Math.Round(Math.Log(x, y), z));
+        }
+        public Loga(float x, float y, int z)
+        {
+            Schot(x, y, z);
+        }
+    }
+    public class ProgramMain
+    {
+        static void Main()
+        {
+            Loga.Input();            
+            float x = float.Parse(Console.ReadLine());
+            float y = float.Parse(Console.ReadLine());
+            int z = int.Parse(Console.ReadLine());
+            Loga program1 = new Loga(x, y, z);
+        }
+    }
+}
